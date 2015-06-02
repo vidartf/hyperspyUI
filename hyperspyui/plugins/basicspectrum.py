f3eb462348acc8f7297a592c4dde82c4418ee056
vidartf <vidartf@gmail.com>
2015-06-02 18:02:08 +0200

    Background ROI tool, WIP

--- hyperspyui/plugins/basicspectrum.py
+++ hyperspyui/plugins/basicspectrum.py
@@ -13,8 +13,8 @@ from QtGui import *
 
 from hyperspyui.widgets.elementpicker import ElementPickerWidget
 from hyperspyui.threaded import Threaded
-from hyperspyui.util import win2sig
-from hyperspyui.tools import SelectionTool
+from hyperspyui.util import win2sig, load_cursor
+from hyperspyui.tools import SelectionTool, SignalFigureTool
 
 import hyperspy.signals
 from hyperspy.misc.eds.utils import _get_element_and_line
@@ -105,6 +105,13 @@ class BasicSpectrumPlugin(Plugin):
                               self.ui))
         except ImportError:
             pass
+        self.background_tool = EdsBackgroundwindowTool()
+        self.add_tool(self.background_tool,
+                      SignalTypeFilter(
+                          (hyperspy.signals.EELSSpectrum,
+                           hyperspy.signals.EDSSEMSpectrum,
+                           hyperspy.signals.EDSTEMSpectrum),
+                          self.ui))
 
     def _toggle_fixed_height(self, floating):
         w = self.picker_widget
@@ -212,3 +219,72 @@ class ElementPickerTool(SelectionTool):
             m.exec_(QCursor.pos())
 
         self.cancel()
+
+
+class EdsBackgroundwindowTool(SignalFigureTool):
+
+#    cancelled = QtCore.Signal()
+
+    def __init__(self, windows=None):
+        super(EdsBackgroundwindowTool, self).__init__(windows)
+
+    def get_name(self):
+        return "EDS background window tool"
+
+    def get_category(self):
+        return 'EDS'
+#
+#    def get_icon(self):
+#        return os.path.dirname(__file__) + '/../images/periodic_table.svg'
+
+    def make_cursor(self):
+        return load_cursor(os.path.dirname(__file__) +
+                           '/../images/picker.svg', 8, 8)
+
+    def on_mousedown(self, event):
+        s = self._get_signal(event.canvas.figure)
+        if s is None:
+            return
+        xray_lines = s._get_xray_lines()
+        print xray_lines
+        if not hasattr(s, '_add_background_windows_rois'):
+            return
+        if not hasattr(s, '_background_window_rois'):
+            s._background_window_rois = {}
+        if s._background_window_rois.keys() != xray_lines:
+            bwml = s._background_window_rois.keys()
+            to_remove = set(bwml) - set(xray_lines)
+            to_add = list(set(xray_lines) - set(bwml))
+            for line in to_remove:
+                g = s._background_window_rois.pop(line)
+                for m in g[1]:
+                    m.close()
+                for r in g[0]:
+                    for w, _ in r.signal_map:
+                        w.close()
+                    r.signal_map.clear()
+            bw = s.estimate_background_windows(xray_lines=to_add)
+            rois, markers = s._add_background_windows_rois(bw)
+            for i, xl in enumerate(to_add):
+                s._background_window_rois[xl] = (rois[i], markers[i])
+
+    def estimate_background(self, signal=None):
+        if signal is None:
+            signal = self._get_signal(gcf())
+            if signal is None:
+                return
+        xray_lines = signal._get_xray_lines()
+        bw = signal.estimate_background_windows(xray_lines=xray_lines)
+        rois, markers = signal._add_background_windows_rois(bw)
+        for i, xl in enumerate(to_add):
+            s._background_window_rois[xl] = (rois[i], markers[i])
+
+    def is_selectable(self):
+        return True
+
+#    def cancel(self):
+#        self.cancelled.emit()
+#
+#    def disconnect_windows(self, windows):
+#        super(EdsBackgroundwindowTool, self).disconnect_windows(windows)
+#        self.cancel()
